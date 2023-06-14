38ad10d46322087044ef49c3b41367374c605359
Lucas Peyrin <129959499+lucaspeyrin@users.noreply.github.com>
2023-06-14 11:47:44 +0200

    Update test.py

--- test.py
+++ test.py
@@ -22,21 +22,24 @@ def show_page1():
     st.write("Liste de todos de la page 1 :")
     todos = ["Tâche 1", "Tâche 2", "Tâche 3"]
     for todo in todos:
-        st.write(f"- {todo}")
+        todo_status = st.checkbox(todo)
+        st.write(f"- {todo} ({todo_status})")
 
 def show_page2():
     st.header("Page 2")
     st.write("Liste de todos de la page 2 :")
     todos = ["Tâche A", "Tâche B", "Tâche C"]
     for todo in todos:
-        st.write(f"- {todo}")
+        todo_status = st.checkbox(todo)
+        st.write(f"- {todo} ({todo_status})")
 
 def show_page3():
     st.header("Page 3")
     st.write("Liste de todos de la page 3 :")
     todos = ["Tâche X", "Tâche Y", "Tâche Z"]
     for todo in todos:
-        st.write(f"- {todo}")
+        todo_status = st.checkbox(todo)
+        st.write(f"- {todo} ({todo_status})")
 
 if __name__ == "__main__":
     main()
