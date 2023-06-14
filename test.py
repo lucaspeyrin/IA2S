5a95e22d23aec4742902e925ab99c5365a3d39b3
IA2S <lucas.peyrin@gmail.com>
2023-06-14 15:18:29 +0000

    Mise à jour de test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ hide_streamlit_style = """
             footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(hide_streamlit_style, unsafe_allow_html=False) 
 
 def afficher_taches(taches):
     for tache in taches:
