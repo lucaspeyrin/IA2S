0aa5ef973149770eb229679564cd13a670b8b0c5
IA2S <lucas.peyrin@gmail.com>
2023-06-14 14:45:06 +0000

    Mise à jour de test.py

--- test.py
+++ test.py
@@ -16,7 +16,6 @@ hide_streamlit_style = """
     display: none !important;
 }
             #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
