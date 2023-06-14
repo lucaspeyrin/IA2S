614d82e0e9f78bdcb92a19d6dcb83f4fcc8f86ed
Lucas Peyrin <129959499+lucaspeyrin@users.noreply.github.com>
2023-06-14 11:43:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,42 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.title('Uber pickups in NYC')
+def main():
+    # Titre de l'application
+    st.title("Application de Todos")
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+    # Création d'une barre latérale pour la navigation entre les pages
+    st.sidebar.title("Navigation")
+    pages = ["Page 1", "Page 2", "Page 3"]
+    selected_page = st.sidebar.radio("Aller à", pages)
 
-df
+    # Affichage de la page sélectionnée
+    if selected_page == "Page 1":
+        show_page1()
+    elif selected_page == "Page 2":
+        show_page2()
+    elif selected_page == "Page 3":
+        show_page3()
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+def show_page1():
+    st.header("Page 1")
+    st.write("Liste de todos de la page 1 :")
+    todos = ["Tâche 1", "Tâche 2", "Tâche 3"]
+    for todo in todos:
+        st.write(f"- {todo}")
 
-st.line_chart(chart_data)
+def show_page2():
+    st.header("Page 2")
+    st.write("Liste de todos de la page 2 :")
+    todos = ["Tâche A", "Tâche B", "Tâche C"]
+    for todo in todos:
+        st.write(f"- {todo}")
+
+def show_page3():
+    st.header("Page 3")
+    st.write("Liste de todos de la page 3 :")
+    todos = ["Tâche X", "Tâche Y", "Tâche Z"]
+    for todo in todos:
+        st.write(f"- {todo}")
+
+if __name__ == "__main__":
+    main()
