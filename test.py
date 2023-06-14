db6423a5d9450fbdf71ce1786c97e921a0a366bb
Lucas Peyrin <129959499+lucaspeyrin@users.noreply.github.com>
2023-06-14 11:51:00 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,6 @@
 import streamlit as st
-from streamlit.report_thread import get_report_ctx
-from streamlit.server.server import Server
-
-# Classe pour gérer l'état de session
-class SessionState:
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-# Fonction pour obtenir l'état de session
-def get_state():
-    session_id = get_report_ctx().session_id
-    session_state = Server.get_current()._session_infos[session_id].session_state
-    return session_state
 
 def main():
-    session_state = get_state()
-
     # Titre de l'application
     st.title("Application de Todos")
 
@@ -25,46 +10,46 @@ def main():
     selected_page = st.sidebar.radio("Aller à", pages)
 
     # Initialisation de l'état de session pour chaque page
-    if not hasattr(session_state, "page1_todos"):
-        session_state.page1_todos = ["Tâche 1", "Tâche 2", "Tâche 3"]
-    if not hasattr(session_state, "page2_todos"):
-        session_state.page2_todos = ["Tâche A", "Tâche B", "Tâche C"]
-    if not hasattr(session_state, "page3_todos"):
-        session_state.page3_todos = ["Tâche X", "Tâche Y", "Tâche Z"]
+    if "page1_todos" not in st.session_state:
+        st.session_state.page1_todos = ["Tâche 1", "Tâche 2", "Tâche 3"]
+    if "page2_todos" not in st.session_state:
+        st.session_state.page2_todos = ["Tâche A", "Tâche B", "Tâche C"]
+    if "page3_todos" not in st.session_state:
+        st.session_state.page3_todos = ["Tâche X", "Tâche Y", "Tâche Z"]
 
     # Affichage de la page sélectionnée
     if selected_page == "Page 1":
-        show_page1(session_state.page1_todos)
+        show_page1()
     elif selected_page == "Page 2":
-        show_page2(session_state.page2_todos)
+        show_page2()
     elif selected_page == "Page 3":
-        show_page3(session_state.page3_todos)
+        show_page3()
 
 # Fonction pour afficher la page 1
-def show_page1(todos):
+def show_page1():
     st.header("Page 1")
     st.write("Liste de todos de la page 1 :")
-    for i, todo in enumerate(todos):
+    for i, todo in enumerate(st.session_state.page1_todos):
         todo_status = st.checkbox(f"{todo}", value=False, key=f"page1_{i}")
-        todos[i] = todo_status
+        st.session_state.page1_todos[i] = todo_status
         st.write(f"- {todo} ({todo_status})")
 
 # Fonction pour afficher la page 2
-def show_page2(todos):
+def show_page2():
     st.header("Page 2")
     st.write("Liste de todos de la page 2 :")
-    for i, todo in enumerate(todos):
+    for i, todo in enumerate(st.session_state.page2_todos):
         todo_status = st.checkbox(f"{todo}", value=False, key=f"page2_{i}")
-        todos[i] = todo_status
+        st.session_state.page2_todos[i] = todo_status
         st.write(f"- {todo} ({todo_status})")
 
 # Fonction pour afficher la page 3
-def show_page3(todos):
+def show_page3():
     st.header("Page 3")
     st.write("Liste de todos de la page 3 :")
-    for i, todo in enumerate(todos):
+    for i, todo in enumerate(st.session_state.page3_todos):
         todo_status = st.checkbox(f"{todo}", value=False, key=f"page3_{i}")
-        todos[i] = todo_status
+        st.session_state.page3_todos[i] = todo_status
         st.write(f"- {todo} ({todo_status})")
 
 if __name__ == "__main__":
