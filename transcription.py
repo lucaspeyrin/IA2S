41a8fba198882321f70a53e84caddc95e9180c3b
Lucas Peyrin <129959499+lucaspeyrin@users.noreply.github.com>
2024-04-28 20:38:31 +0200

    Update transcription.py

--- transcription.py
+++ transcription.py
@@ -12,6 +12,10 @@ if 'coordinates' not in st.session_state:
     st.session_state.coordinates = None
     st.session_state.ignore = False
 
+# Calculer la hauteur affichée en fonction de la largeur affichée de 300 pixels
+displayed_height = int((st.session_state.image_height / st.session_state.image_width) * 300)
+displayed_width = 300
+
 # Fonction pour récupérer les données d'image de l'API
 def get_image_data_from_api(coordinates):
     api_url = "https://api.ia2s.app/webhook/streamlit/screenshot"
@@ -22,8 +26,8 @@ def get_image_data_from_api(coordinates):
 # Fonction pour calculer les coordonnées en pourcentage
 def calculate_percentage_coordinates(coordinates, image_width, image_height):
     if coordinates:
-        x_percentage = (coordinates["x"] / image_width) * 100
-        y_percentage = 100 - ((coordinates["y"] / image_height) * 100)
+        x_percentage = (coordinates["x"] / displayed_width) * 100
+        y_percentage = (coordinates["y"] / displayed_height) * 100)
         return {"x": x_percentage, "y": y_percentage}
     else:
         return {}
@@ -50,10 +54,6 @@ elif st.session_state.coordinates is not None and st.session_state.ignore is not
     st.session_state.image_url, st.session_state.image_width, st.session_state.image_height = get_image_data_from_api(st.session_state.percentage_coordinates)
     st.write("Exécution suivante")
 
-
-# Calculer la hauteur affichée en fonction de la largeur affichée de 300 pixels
-displayed_height = int((st.session_state.image_height / st.session_state.image_width) * 300)
-displayed_width = 300
 
 # Affichage de l'image avec les coordonnées
 coordinates = streamlit_image_coordinates(
