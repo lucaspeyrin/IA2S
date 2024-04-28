6414397b5bc7466401697bae8f27b6b3de9f0bc4
Lucas Peyrin <129959499+lucaspeyrin@users.noreply.github.com>
2024-04-28 18:48:29 +0200

    Update transcription.py

--- transcription.py
+++ transcription.py
@@ -46,9 +46,10 @@ def calculate_percentage_coordinates(coordinates, image_width, image_height):
 
 "Try clicking on the image below."
 
+st.session_state.image_url, st.session_state.image_width, st.session_state.image_height = get_image_data_from_api(st.session_state.percentage_coordinates)
+
 st.session_state.percentage_coordinates = calculate_percentage_coordinates(st.session_state.coordinates, st.session_state.image_width, st.session_state.image_height)
 
-st.session_state.image_url, st.session_state.image_width, st.session_state.image_height = get_image_data_from_api(st.session_state.percentage_coordinates)
 
 # Calculate the displayed height based on the displayed width of 300 pixels
 displayed_height = int((st.session_state.image_height / st.session_state.image_width) * 300)
