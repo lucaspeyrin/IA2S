f28b3324e3579433a0eb48c407d43958ea5a29ec
Lucas Peyrin <129959499+lucaspeyrin@users.noreply.github.com>
2024-04-28 19:08:40 +0200

    Update transcription.py

--- transcription.py
+++ transcription.py
@@ -36,17 +36,6 @@ def calculate_percentage_coordinates(coordinates, image_width, image_height):
 # Affichage de l'interface utilisateur Streamlit
 st.title("Streamlit Image Coordinates")
 
-# Calculer les coordonnées en pourcentage en premier
-st.session_state.percentage_coordinates = calculate_percentage_coordinates(st.session_state.coordinates, st.session_state.image_width, st.session_state.image_height)
-
-# Obtenir les données d'image de l'API en utilisant les coordonnées en pourcentage
-if st.session_state.coordinates is None:
-    st.session_state.image_url, st.session_state.image_width, st.session_state.image_height = get_image_data_from_api({})
-    st.write("First execution")
-else:
-    st.session_state.image_url, st.session_state.image_width, st.session_state.image_height = get_image_data_from_api(st.session_state.percentage_coordinates)
-    st.write("Next execution")
-
 # Calculer la hauteur affichée en fonction de la largeur affichée de 300 pixels
 displayed_height = int((st.session_state.image_height / st.session_state.image_width) * 300)
 displayed_width = 300
@@ -61,6 +50,22 @@ st.session_state.coordinates = streamlit_image_coordinates(
 
 # Affichage des coordonnées
 st.write(st.session_state.coordinates)
+
+# Calculer les coordonnées en pourcentage en premier
+st.session_state.percentage_coordinates = calculate_percentage_coordinates(st.session_state.coordinates, st.session_state.image_width, st.session_state.image_height)
+
+# Obtenir les données d'image de l'API en utilisant les coordonnées en pourcentage
+if st.session_state.coordinates is None:
+    st.session_state.image_url, st.session_state.image_width, st.session_state.image_height = get_image_data_from_api({})
+    st.write("First execution")
+else:
+    st.session_state.image_url, st.session_state.image_width, st.session_state.image_height = get_image_data_from_api(st.session_state.percentage_coordinates)
+    st.write("Next execution")
+
+
+
+
+
 
 st.title('Counter Example using Callbacks with args')
 if 'count' not in st.session_state:
