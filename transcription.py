abf887483c416b11c3a3c716762704d996ebbbf3
Lucas Peyrin <129959499+lucaspeyrin@users.noreply.github.com>
2024-04-28 19:27:26 +0200

    Update transcription.py

--- transcription.py
+++ transcription.py
@@ -33,9 +33,19 @@ def calculate_percentage_coordinates(coordinates, image_width, image_height):
     else:
         return {}
 
+# Définir une fonction de rappel pour mettre à jour les variables en fonction des coordonnées
+@st.cache(suppress_st_warning=True)
+def update_variables():
+    st.session_state.image_url, st.session_state.image_width, st.session_state.image_height = get_image_data_from_api(st.session_state.percentage_coordinates)
+    st.session_state.coordinates = streamlit_image_coordinates(
+        st.session_state.image_url,
+        width=displayed_width,
+        height=displayed_height,
+        key="url",
+    )
+
 # Affichage de l'interface utilisateur Streamlit
 st.title("Streamlit Image Coordinates")
-
 
 # Calculer les coordonnées en pourcentage en premier
 st.session_state.percentage_coordinates = calculate_percentage_coordinates(st.session_state.coordinates, st.session_state.image_width, st.session_state.image_height)
@@ -45,22 +55,12 @@ if st.session_state.coordinates is None:
     st.session_state.image_url, st.session_state.image_width, st.session_state.image_height = get_image_data_from_api({})
     st.write("First execution")
 else:
-    st.session_state.image_url, st.session_state.image_width, st.session_state.image_height = get_image_data_from_api(st.session_state.percentage_coordinates)
+    update_variables()
     st.write("Next execution")
 
 # Calculer la hauteur affichée en fonction de la largeur affichée de 300 pixels
 displayed_height = int((st.session_state.image_height / st.session_state.image_width) * 300)
 displayed_width = 300
 
-# Affichage de l'image avec les coordonnées
-st.session_state.coordinates = streamlit_image_coordinates(
-    st.session_state.image_url,
-    width=displayed_width,
-    height=displayed_height,
-    key="url",
-    sync=True,
-)
-
 # Affichage des coordonnées
 st.write(st.session_state.coordinates)
-    
