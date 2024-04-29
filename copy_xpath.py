54a48b0f6104524695939831b4aef3686840388a
Lucas Peyrin <129959499+lucaspeyrin@users.noreply.github.com>
2024-04-29 12:00:30 +0200

    Update copy_xpath.py

--- copy_xpath.py
+++ copy_xpath.py
@@ -66,10 +66,6 @@ st.title("Phone Id")
 # Input pour le phone id
 st.session_state.phone_id = st.text_input("Phone Id", st.session_state.phone_id)
 
-if st.session_state.image_url is None:
-    if st.button("Start"):
-        st.session_state.ignore = False
-        st.session_state.image_url, st.session_state.image_width, st.session_state.image_height, st.session_state.layout = get_image_data_from_api(st.session_state.phone_id)
 
 if st.session_state.ignore is not True and st.session_state.image_url is None and st.session_state.phone_id is not None:
     st.session_state.image_url, st.session_state.image_width, st.session_state.image_height, st.session_state.layout = get_image_data_from_api(st.session_state.phone_id)
@@ -133,3 +129,7 @@ if st.session_state.image_url:
                 st.subheader(action["name"])
                 st.code(action["xpath"])
         
+if st.session_state.image_url is None:
+    if st.button("Start"):
+        st.session_state.ignore = False
+        st.session_state.image_url, st.session_state.image_width, st.session_state.image_height, st.session_state.layout = get_image_data_from_api(st.session_state.phone_id)
