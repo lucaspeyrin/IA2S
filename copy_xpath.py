1633ec77a4ab4158ebf72a40f856c2d487699c26
Lucas Peyrin <129959499+lucaspeyrin@users.noreply.github.com>
2024-11-07 14:38:14 +0100

    Update copy_xpath.py

--- copy_xpath.py
+++ copy_xpath.py
@@ -131,7 +131,7 @@ with col1:
        # Affichage des coordonnées
         st.write(coordinates)
         st.session_state.coordinates = coordinates
-        
+        st.write(st.session_state.coordinates)
         if st.session_state.coordinates:
             st.session_state.ignore = False
             st.session_state.percentage_coordinates = calculate_percentage_coordinates(st.session_state.coordinates, st.session_state.image_width, st.session_state.image_height)
