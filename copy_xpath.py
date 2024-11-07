5a6cf689e9b2601f6d5a8dc88918b78f3899a7d5
Lucas Peyrin <129959499+lucaspeyrin@users.noreply.github.com>
2024-11-07 14:26:37 +0100

    Update copy_xpath.py

--- copy_xpath.py
+++ copy_xpath.py
@@ -129,6 +129,10 @@ with col1:
             img, width=displayed_width, height=displayed_height, key="url"
         )
 
+        if st.session_state.coordinates:
+                st.session_state.ignore = False
+                st.session_state.percentage_coordinates = calculate_percentage_coordinates(st.session_state.coordinates, st.session_state.image_width, st.session_state.image_height)
+                
         # Mise à jour des coordonnées dans le session state si l'utilisateur clique
         if coordinates is not None:
             # Calculer les coordonnées correctes par rapport à la taille réelle de l'image
@@ -138,9 +142,6 @@ with col1:
             point = x_real, y_real
             if point not in st.session_state["points"]:
                 st.session_state["points"].append(point)
-                
-                # Calcul des coordonnées en pourcentage
-                st.session_state.percentage_coordinates = calculate_percentage_coordinates(coordinates, st.session_state.image_width, st.session_state.image_height)
                 
                 # Rafraîchir l'interface avec les nouvelles coordonnées
                 st.rerun()
